45edcfb2d5080217a6fb9fd29d146e950e357271
radiatom <arsenmozol@gmail.com>
2022-12-28 18:00:36 +0200

    21 Dialogs update

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -5,8 +5,52 @@ import s from './Dialogs.module.css'
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
-            <p>Dialogs</p>
+            <div className={s.dialogsItems}>
+                <div className={s.active}>
+                    Arsen
+                </div>
+
+                <div className={s.dialog}>
+                    Mycola
+                </div>
+
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+
+                <div className={s.dialog}>
+                    Igor
+                </div>
+
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+
+                <div className={s.dialog}>
+                    Antony
+                </div>
+            </div>
+
+            <div className={s.massages}>
+                
+                    <div className={s.dialog}>
+                        Hi
+                    </div>
+
+                    <div className={s.dialog}>
+                        Hi
+                    </div>
+
+                    <div className={s.dialog}>
+                        Its you admin?
+                    </div>
+
+                    <div className={s.dialog}>
+                        Yes
+                    </div>
+                
         </div>
+        </div >
     );
 }
 
