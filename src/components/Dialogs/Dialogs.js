ab407deb655c68ca9bf40927be77b8d2d1d6cb54
radiatom <arsenmozol@gmail.com>
2022-12-29 16:09:36 +0200

    25 data.map(el=>{return(el.)})

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,40 +3,43 @@ import s from './Dialogs.module.css'
 import Massage from './Massage/Massage';
 import Dialog from './Dialog/Dialog';
 
-const dialogData=[
-    {id:1,name:'Arsen'},
-    {id:2,name:'Mycola'},
-    {id:3,name:'Andrey'},
-    {id:4,name:'Igor'},
-    {id:5,name:'Viktor'},
-    {id:6,name:'Antony'},
+const dialogData = [
+    { id: 1, name: 'Arsen' },
+    { id: 2, name: 'Mycola' },
+    { id: 3, name: 'Andrey' },
+    { id: 4, name: 'Igor' },
+    { id: 5, name: 'Viktor' },
+    { id: 6, name: 'Antony' },
+    { id: 7, name: 'Bot' },
+    { id: 8, name: 'Roma' },
+
 ]
 
-const massageData=[
-    {id:1,massage:'Hi'},
-    {id:2,massage:'Hi'},
-    {id:3,massage:'Its you admin?'},
-    {id:4,massage:'Yes'},
+const massagesData = [
+    { id: 1, massage: 'Hi' },
+    { id: 2, massage: 'Hi' },
+    { id: 3, massage: 'Its you admin?' },
+    { id: 4, massage: 'Yes' },
+    { id: 4, massage: 'Cool!' }
 ]
+
+const dialogsItem = dialogData.map(el => {
+    return (<Dialog name={el.name} id={el.id} />)
+})
+
+const massages = massagesData.map(el => {
+    return (<Massage massage={el.massage} />)
+})
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <Dialog name={dialogData[0].name} id={dialogData[0].id} />
-                <Dialog name={dialogData[1].name} id={dialogData[1].id} />
-                <Dialog name={dialogData[2].name} id={dialogData[2].id} />
-                <Dialog name={dialogData[3].name} id={dialogData[3].id} />
-                <Dialog name={dialogData[4].name} id={dialogData[4].id} />
-                <Dialog name={dialogData[5].name} id={dialogData[5].id} />
+                {dialogsItem}
             </div>
 
             <div className={s.massages}>
-                <Massage massage={massageData[0].massage}/>
-                <Massage massage={massageData[1].massage}/>
-                <Massage massage={massageData[2].massage}/>
-                <Massage massage={massageData[3].massage}/>
-
+                {massages}
             </div>
         </div >
     );
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -2,12 +2,13 @@ import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
-const postData=[
-      {id:1,massage:'Hi, how a you?',likeColum:15},
-      {id:2,massage:'Its my first post',likeColum:999}
-
-
+const postData = [
+      { id: 1, massage: 'Hi, how a you?', likeColum: 15 },
+      { id: 2, massage: 'Its my first post', likeColum: 999 }
 ]
+const posts = postData.map(el => {
+      return (<Post massage={el.massage} likeColum={el.likeColum} />)
+})
 
 const MyPosts = (props) => {
       return (
@@ -17,11 +18,7 @@ const MyPosts = (props) => {
                   <button>New post</button>
 
                   <div >
-                        <Post massage={postData[0].massage} likeColum={postData[0].likeColum}/>
-
-                        <Post massage={postData[1].massage} likeColum={postData[1].likeColum}/>
-
-                        
+                        {posts}
                   </div>
             </div>
       );
