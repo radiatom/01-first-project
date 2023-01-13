a42acf1d5db39ae7758e0db5d89771c92063d448
radiatom <arsenmozol@gmail.com>
2023-01-13 22:37:09 +0200

    52 api axios.get

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
-
+import img from './../../../Img/profileInfo.jpg'
 
 const ProfileInfo = (props) => {
     return (
         <div className={s.profileInfo}>
             <div>
-                <img src="https://as2.ftcdn.net/v2/jpg/02/99/73/07/1000_F_299730781_D2CZYaxecEerqAAlRZZushSUml5Tm5Kq.jpg" alt="img" />
+                <img src={img} alt="img" />
             </div>
             <div>Distription</div>
         </div>
