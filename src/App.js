331cbb5618c7c9ae72e1bf90e4e124a2ee43607b
radiatom <arsenmozol@gmail.com>
2023-02-05 15:32:49 +0200

    94 React.lazy() , <Suspense>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route, } from "react-router-dom";
 import "./App.css";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Navbar from "./components/Navbar/Navbar";
-import MyProfileContainer from "./components/MyProfile/MyProfileContainer";
-import Music from "./components/Music/Music";
-import News from "./components/News/News";
-import SettingContainer from "./components/Setting/SettingContainer";
-import MyFriendsContainer from "./components/MyFriends/MyFriendsContainer";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersListContainer from "./components/UsersList/UsersListContainer";
-import UserProfileContainer from "./components/UserProfile/UserProfileContainer";
-import SingInFormContainer from "./components/SingInForm/SingInFormContainer";
 import { getAuth } from "./redux/authReducer";
 import { connect } from "react-redux";
 import PreloaderEntrance from "./components/common/Preloader/PreloaderEntrance";
+import Preloader from "./components/common/Preloader/Preloader";
+
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import SettingContainer from "./components/Setting/SettingContainer";
+// import UsersListContainer from "./components/UsersList/UsersListContainer";
+// import MyProfileContainer from "./components/MyProfile/MyProfileContainer";
+import MyFriendsContainer from "./components/MyFriends/MyFriendsContainer";
+import SingInFormContainer from "./components/SingInForm/SingInFormContainer";
+// import UserProfileContainer from "./components/UserProfile/UserProfileContainer";
+
+// React.lazy лінива підгрузка, лише тоді коли цю компоненту трогають тоді вона буде підгружатись. 
+// Стандартно реакт підгружає все підряд що не обернене в lazy.
+// const News=React.lazy(()=> import("./components/News/News"))
+// const Music=React.lazy(()=> import("./components/Music/Music"))
+const DialogsContainer=React.lazy(()=> import("./components/Dialogs/DialogsContainer"))
+// const SettingContainer=React.lazy(()=> import("./components/Setting/SettingContainer"))
+const UsersListContainer=React.lazy(()=> import("./components/UsersList/UsersListContainer"))
+const MyProfileContainer=React.lazy(()=> import("./components/MyProfile/MyProfileContainer"))
+// const MyFriendsContainer=React.lazy(()=> import("./components/MyFriends/MyFriendsContainer"))
+// const SingInFormContainer=React.lazy(()=> import("./components/SingInForm/SingInFormContainer"))
+const UserProfileContainer=React.lazy(()=> import('./components/UserProfile/UserProfileContainer'))
 
 
 const App = (props) => {
@@ -23,10 +37,11 @@ const App = (props) => {
             <div className='preloader'>
                   <PreloaderEntrance />
             </div>
-                  :
-                  <div className="app-wrapper">
-                        <HeaderContainer />
-                        <Navbar />
+            :
+            <div className="app-wrapper">
+                  <HeaderContainer />
+                  <Navbar />
+                  <Suspense fallback={<Preloader />}>
                         <Routes>
                               <Route path="/singIn/*" element={<SingInFormContainer />} />
                               <Route path="/myFriends/*" element={<MyFriendsContainer />} />
@@ -38,7 +53,8 @@ const App = (props) => {
                               <Route path="/users/*" element={<UsersListContainer />} />
                               <Route path="/userProfile/:userId" element={<UserProfileContainer />} />
                         </Routes>
-                  </div>
+                  </Suspense>
+            </div>
 }
 
 
--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import preloader from '../../../Img/PreloaderEnance.gif'
+import s from './Preloader.module.css'
 
 const Preloader = (props) => {
     return (
-        <div>
+        <div className={s.preloader}>
             <img src={preloader} alt="preloader" />
         </div>
     );
