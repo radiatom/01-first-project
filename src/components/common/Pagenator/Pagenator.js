893041775ac01b8a9cacf8bb6718bd59240c4458
radiatom <arsenmozol@gmail.com>
2023-02-05 13:06:21 +0200

    93 pagenator colum portion

--- src/components/common/Pagenator/Pagenator.js
+++ src/components/common/Pagenator/Pagenator.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Pagenator.module.css'
 
+
 const Pagenator = (props) => {
+    const [portionNumber, setPortionNumber] = useState(1)
+    const leftPortion = () => {
+        setPortionNumber(portionNumber - 1)
+    }
+    const rightPortion = () => {
+        setPortionNumber(portionNumber + 1)
+    }
+
+    const portionSize = 12
     const pages = Math.ceil(props.countUsers / props.countUsersOnPage)
+    const columPortion = Math.ceil(pages / portionSize)
+
     const pagesCounts = []
-    for (let a = 1; a <= pages; a++) {
+    for (let a = 1 + ((portionNumber - 1) * portionSize); a <= portionNumber * portionSize && a <= pages && a >= 1; a++) {
         pagesCounts.push(a)
     }
     return (
         <div className={s.pages}>
+            <span>
+                {portionNumber > 1 ? <button onClick={leftPortion}>PREW</button> : ''}
+            </span>
+
             <span className={s.page} >
                 {pagesCounts.map(el => {
                     return (
@@ -20,6 +36,10 @@ const Pagenator = (props) => {
                             {el}
                         </span>)
                 })}
+            </span>
+
+            <span>
+                {portionNumber !== columPortion ? <button onClick={rightPortion}>NEXT</button> : ''}
             </span>
         </div>
     );
