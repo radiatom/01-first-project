4cc781697c1ed289e1bf6e7a9b6cd8db24993619
radiatom <arsenmozol@gmail.com>
2023-02-08 15:26:29 +0200

    Proect is fine

--- src/index.js
+++ src/index.js
@@ -10,21 +10,21 @@ import store from './redux/redux_store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    // <HashRouter>
-    //     <Provider store={store}>
-    //         <React.StrictMode>
-    //             <App />
-    //         </React.StrictMode>
-    //     </Provider>
-    // </HashRouter>
-    // <HashRouter> треба щоб гітхаб коректно ссилкі обробляв
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
+    // <HashRouter> треба щоб гітхаб коректно ссилкі обробляв
+    // <BrowserRouter>
+    //     <Provider store={store}>
+    //         <React.StrictMode>
+    //             <App />
+    //         </React.StrictMode>
+    //     </Provider>
+    // </BrowserRouter>
     // {/* <BrowserRouter> це правильна дорога для побудови додатку */}
 );
 
